8b67f1e4b2e8000485d3ff2cdc3b1c3ad11cdb58
Rafael Pinto <rafaelgcpp@gmail.com>
2026-02-01 22:23:36 -0300

    fixed forward rfft.

--- src/float/real.rs
+++ src/float/real.rs
@@ -1,12 +1,12 @@
+use super::core::{precompute_bitrev, precompute_twiddles, radix_2_dit_fft_core};
 use crate::common::FftError;
-use num_complex::Complex32;
 use core::slice;
-use super::core::{radix_2_dit_fft_core, precompute_twiddles, precompute_bitrev};
+use num_complex::Complex32;
 
-#[cfg(feature = "std")]
-use std::f32;
 #[cfg(not(feature = "std"))]
 use libm::Libm;
+#[cfg(feature = "std")]
+use std::f32;
 
 pub struct RealFft<'a> {
     twiddles: &'a mut [Complex32],
@@ -18,9 +18,9 @@ impl<'a> RealFft<'a> {
     /// Initializes the Real FFT.
     /// Note that 'n' here is the number of REAL samples.
     pub fn new(
-        twiddles: &'a mut [Complex32], 
-        bitrev: &'a mut [usize], 
-        n: usize
+        twiddles: &'a mut [Complex32],
+        bitrev: &'a mut [usize],
+        n: usize,
     ) -> Result<Self, FftError> {
         if !n.is_power_of_two() {
             return Err(FftError::NotPowerOfTwo);
@@ -33,15 +33,19 @@ impl<'a> RealFft<'a> {
             return Err(FftError::BufferTooSmall);
         }
 
-        let mut fft = Self { twiddles, bitrev, n };
+        let mut fft = Self {
+            twiddles,
+            bitrev,
+            n,
+        };
         fft.precompute();
         Ok(fft)
     }
 
-    fn precompute(&mut self) {        
+    fn precompute(&mut self) {
         // 1. Bitrev is generated for N/2 (size of the internal FFT)
         precompute_bitrev(self.bitrev, self.n / 2);
-        
+
         // 2. Twiddles are generated for N (full circle)
         // This is what allows the post-processing to work
         precompute_twiddles(self.twiddles, self.n);
@@ -59,12 +63,8 @@ impl<'a> RealFft<'a> {
 
         // C TRICK: Reinterpret float array as Complex array
         // Safety: Complex32 is repr(C) of two f32s, and the alignment is compatible.
-        let cbuffer = unsafe {
-            slice::from_raw_parts_mut(
-                buffer.as_mut_ptr() as *mut Complex32,
-                self.n / 2
-            )
-        };
+        let cbuffer =
+            unsafe { slice::from_raw_parts_mut(buffer.as_mut_ptr() as *mut Complex32, self.n / 2) };
 
         // 1. Execute Complex FFT of N/2 points
         // Since the internal FFT needs "stride 2" in twiddles (using only even indices
@@ -77,59 +77,16 @@ impl<'a> RealFft<'a> {
         let n_quarter = n_half / 2;
 
         // Process index 0 (DC and Nyquist)
-        {
-            let val = cbuffer[0];
-            let tmp = val.conj();
-            
-            let even = (val + tmp).scale(0.5);
-            let odd = (val - tmp).scale(0.5);
-            
-            // cdata[0] = even - I * odd; 
-            // -I * odd = -I * (odd.re + I*odd.im) = -I*odd.re + odd.im
-            let minus_i_odd = Complex32::new(odd.im, -odd.re); // Multiplication by -i
-            
-            // Storage trick: Real=DC, Imag=Nyquist
-            // In the original C code:
-            // cdata[0] += I * even - odd; (weird, let's follow the algebraic logic from C)
-            // C Code:
-            // tmp.real = -odd.imag; tmp.imag = odd.real; (tmp = i * odd) ?? No, -odd.im is mult by -i if odd is pure real...
-            // Let's strictly follow the lines from C rfft.c:
-            
-            // C: even = (cdata[0] + conj(cdata[0])) / 2;
-            // C: odd = (cdata[0] - conj(cdata[0])) / 2;
-            // C: tmp = I * odd -> (re: -odd.im, im: odd.re)
-            // C: cdata[0] = even - I * odd; -> This recovers the correct value
-            // C: tmp = I * even
-            // C: cdata[0] += I * even - odd; 
-            
-            // Simplifying what C does at the end for index 0:
-            // The C code sets: 
-            // Real part = even.real + odd.imag (Basically the sum of original real parts)
-            // Imag part = even.real - odd.imag
-            // Let's use the direct reconstruction logic:
-            cbuffer[0] = Complex32::new(val.re + val.im, val.re - val.im);
-            // Note: If there's a missing 0.5 scale, we will adjust. 
-            // The C code does many additions and subtractions, but the final result for index 0 is this.
-            // To ensure bit-exactness with C, you could copy line by line, 
-            // but Complex32::new(cbuffer[0].re + cbuffer[0].im, ...) is the classic optimization.
-        }
+        let val = cbuffer[0];
+        
+        cbuffer[0] = Complex32::new(val.re + val.im, val.re - val.im);
+
+        // n/4 point
+        cbuffer[n_quarter] = cbuffer[n_quarter].conj();
 
         // Main unweaving loop
-        for i in 1..=n_quarter { // Includes n_quarter to handle the midpoint if necessary
-            // The C code handles n/4 separately, but let's see the loop
-            if i == n_quarter {
-                 // Special case i = N/4
-                 let val = cbuffer[i];
-                 let tmp = val.conj();
-                 let even = (val + tmp).scale(0.5);
-                 let odd = (val - tmp).scale(0.5);
-                 // cdata[n/4] = even - odd; (direction 1)
-                 cbuffer[i] = even - odd; // In complex, -odd is -1*odd.
-                 // The imaginary part of the result should be 0 theoretically if it is exactly n/4?
-                 // In C code: cplx_sub(cdata[n/4], even, odd);
-                 continue;
-            }
 
+        for i in 1..=n_quarter - 1 {
             let idx_a = i;
             let idx_b = n_half - i;
 
@@ -139,33 +96,28 @@ impl<'a> RealFft<'a> {
 
             // even = (cdata[i] + conj(cdata[n/2-i])) / 2
             let even = (val_a + val_b_conj).scale(0.5);
-            
+
             // odd = (cdata[i] - conj(cdata[n/2-i])) / 2
             let odd = (val_a - val_b_conj).scale(0.5);
 
             // Twiddle calculation
             // C: w = twd[i]; (Note that twd here is the full table of size N/2)
-            let w = self.twiddles[i]; 
+            let w = self.twiddles[i];
 
-            // tmp1 = odd * w
-            let tmp1 = odd * w;
-            
-            // tmp = I * tmp1 (re: -tmp1.im, im: tmp1.re)
-            let tmp = Complex32::new(-tmp1.im, tmp1.re);
+            // tmp = odd * w
+            let tmp = odd * w;
+
+            // tmp = I * tmp (re: -tmp.im, im: tmp.re)
+            let tmp = Complex32::new(-tmp.im, tmp.re);
 
             // cdata[i] = even - I * odd * w  => even - tmp
             cbuffer[idx_a] = even - tmp;
 
-            // cdata[n/2 - i] = conj(cdata[i]) (Symmetry!)
-            // The C code explicitly calculates: cdata[n/2-i] = even1 - ...
-            // But for RFFT, the result of N-i is the conjugate of i.
-            // Let's follow C to ensure compliance:
-            // C calculates even1/odd1 based on idx_b and idx_a.conj().
-            // In math: even1 == even.conj(), odd1 == odd.conj()??
-            // Let's trust the symmetric operation:
-            
-            let val_b_res = (even + tmp).conj(); 
-            cbuffer[idx_b] = val_b_res; 
+            // cdata[n/2 i] = conj(even) - I * conj(odd * w)
+            //               = conj(even + I * odd * w) => conj(even + tmp)
+
+            let val_b_res = (even + tmp).conj();
+            cbuffer[idx_b] = val_b_res;
         }
 
         Ok(())
@@ -177,18 +129,14 @@ impl<'a> RealFft<'a> {
             return Err(FftError::SizeMismatch);
         }
 
-        let cbuffer = unsafe {
-            slice::from_raw_parts_mut(
-                buffer.as_mut_ptr() as *mut Complex32,
-                self.n / 2
-            )
-        };
+        let cbuffer =
+            unsafe { slice::from_raw_parts_mut(buffer.as_mut_ptr() as *mut Complex32, self.n / 2) };
 
         let n_half = self.n / 2;
         let n_quarter = n_half / 2;
 
         // 1. Pre-processing (Weaving) - Inverse of the step above
-        
+
         // Main loop
         for i in 1..n_quarter {
             let idx_a = i;
@@ -197,34 +145,34 @@ impl<'a> RealFft<'a> {
             let val_a = cbuffer[idx_a];
             let val_b = cbuffer[idx_b];
             // The C code uses conjugates here
-            
+
             // Let's simplify: The C inverse code is symmetric but with conjugated W and Sums.
-            // I will omit the exhaustive line-by-line translation here for brevity, 
+            // I will omit the exhaustive line-by-line translation here for brevity,
             // but the logic is to mirror the 'process' loop switching signs and conjugating W.
-            
+
             let even = (val_a + val_b.conj()).scale(0.5);
             let odd = (val_a - val_b.conj()).scale(0.5);
-            
+
             // w = conj(twd[i])
             let w = self.twiddles[i].conj();
-            
+
             let tmp1 = odd * w;
             // tmp = I * tmp1
             let tmp = Complex32::new(-tmp1.im, tmp1.re);
-            
+
             cbuffer[idx_a] = even + tmp;
             cbuffer[idx_b] = (even - tmp).conj();
         }
-        
+
         // Point N/4
         {
-             let val = cbuffer[n_quarter];
-             let tmp = val.conj();
-             let even = (val + tmp).scale(0.5);
-             let odd = (val - tmp).scale(0.5);
-             // tmp = I * odd
-             let tmp_i_odd = Complex32::new(-odd.im, odd.re);
-             cbuffer[n_quarter] = even + tmp_i_odd;
+            let val = cbuffer[n_quarter];
+            let tmp = val.conj();
+            let even = (val + tmp).scale(0.5);
+            let odd = (val - tmp).scale(0.5);
+            // tmp = I * odd
+            let tmp_i_odd = Complex32::new(-odd.im, odd.re);
+            cbuffer[n_quarter] = even + tmp_i_odd;
         }
 
         // Point 0 (DC/Nyquist)
@@ -234,10 +182,9 @@ impl<'a> RealFft<'a> {
             let even_re = (val.re + val.im) * 0.5;
             // odd.real = (val.re - val.im) / 2
             let odd_re = (val.re - val.im) * 0.5;
-            
+
             cbuffer[0] = Complex32::new(even_re, odd_re);
         }
-
 
         // 2. Executes Inverse Complex FFT of N/2 points
         radix_2_dit_fft_core::<true>(cbuffer, self.twiddles, self.bitrev, 2);
@@ -245,4 +192,3 @@ impl<'a> RealFft<'a> {
         Ok(())
     }
 }
-
